86b3e00db0f7ac3a1359bc9c601005989bb9843c
EC2 Default User <ec2-user@ip-172-31-37-141.us-east-2.compute.internal>
2018-07-27 14:13:53 +0000

    make it better

--- public/aboutus/script.js
+++ public/aboutus/script.js
@@ -1,19 +1,17 @@
-// Get the modal
-var modal = document.getElementById('id01');
+var slideIndex = 1;
+showDivs(slideIndex);
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+function plusDivs(n) {
+    showDivs(slideIndex += n);
 }
 
-// Get the modal
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {slideIndex = 1} 
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none"; 
     }
+    x[slideIndex-1].style.display = "block"; 
 }
